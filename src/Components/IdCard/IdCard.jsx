2d1bbf489b87c16b446c21c824b4b2cc3cf6891a
Gabriel Rodriguez <gabz89.gr@gmail.com>
2023-02-21 15:57:54 -0300

    actualizacion 21.02

--- src/Components/IdCard/IdCard.jsx
+++ src/Components/IdCard/IdCard.jsx
@@ -2,6 +2,9 @@ import React from "react"
 import image01 from '../img/image01.png'
 import { Button } from "react-bootstrap"
 import './IdCard.css'
+import GeneralModal from "../GeneralModal/GeneralModal"
+import PrincipalPageForm from "../PrincipalPageForm/PrincipalPageForm"
+import AddStudentForm from "../AddStudentForm/AddStudentForm"
 
 
 
@@ -11,12 +14,18 @@ function IdCard({name,cel,email,dni,_id}){
         <div className="d-flex alert-dismissiblecol-12 col-md-6 col-lg-4">
             <img className="img-fluid" src={image01}/>
             <div className="card-body">
-                <h4 className="card-text">{name}</h4>
-                <p className="card-text text-secondary">{cel}</p>
-                <p className="card-text text-secondary">{email}</p>
-                <p className="card-text text-secondary">{dni}</p>
-                <p className="card-text text-secondary">{_id}</p>
-                <Button variant="primary">boton de edicion de admin</Button>{' '}
+                <h4 className="card-text m-2 ">{name}</h4>
+                <p className="card-text text-secondary m-2">{cel}</p>
+                <p className="card-text text-secondary m-2">{email}</p>
+                <p className="card-text text-secondary m-2">{dni}</p>
+                <p className="card-text text-secondary m-2">{_id}</p>
+                <div className="buttons d-flex g-3">
+                {/* <GeneralModal buttonText="Editar" modalTitle="Edicion de Usuario"  modalBody={<PrincipalPageForm/>} variant="primary"/> */}
+                {/* <GeneralModal buttonText="Editar" modalTitle="editar un usuario"  modalBody={<AddStudentForm/>} variant="primary"/> */}
+                <Button variant="primary">Editar</Button>{' '}
+                <Button variant="primary">Habilitar</Button>{' '}
+                <Button variant="primary">Eliminar</Button>{' '}
+                </div>
             </div>
         </div>
     )
