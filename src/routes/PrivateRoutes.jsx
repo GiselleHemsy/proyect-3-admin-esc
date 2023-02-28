d912621e64dc49764bc3c2246d7aeca89be370e7
Agos Mercado <agosmercado@gmail.com>
2023-02-27 21:32:37 -0300

    probando autenticacion

--- src/routes/PrivateRoutes.jsx
+++ src/routes/PrivateRoutes.jsx
@@ -1,30 +1,36 @@
 import { useEffect, useState } from "react";
+import { Spinner } from "react-bootstrap";
 import { Navigate } from "react-router-dom";
 import { toast } from "react-toastify";
 import axiosBack from "../config/axios";
 
 const PrivateRoute = ({children}) => {
   const [user, setUser] = useState(false)
+  const [loading, setLoading] = useState(true)
   const getAuth = async()=>{
     try {
       const token = localStorage.getItem("token");
-      if(!token) return setUser(false);
+      if(!token){
+        setLoading(false)
+        return setUser(false);
+      } 
       axiosBack.defaults.headers.common["authorization"]= token;
       // axiosInstance.defaults.headers.common['authorization'] = token;
       const {data}=await axiosBack.get("/users/authStatus")
       setUser(data.user)
+      console.log(data.users);
+      setLoading(false)
     } catch (error) {
       toast.error("error")
     }
+    setLoading(false)
   }
   // Authorization
   useEffect(()=>{
     getAuth();
   }, [])
   console.log(user)
-  return ( 
-    !!user? children : <Navigate to="/LoginPage"/>
-    );
+  return loading? <Spinner/> : !!user? children : <Navigate to="/LoginPage"/>;
 }
 
 export default PrivateRoute;
